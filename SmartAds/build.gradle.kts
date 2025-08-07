f5e5795e97708d568f1d8f9f4df856ff556d8694
Partha Roy <partharoypc@gmail.com>
2025-08-07 18:38:32 +0600

    Fix: JitPack support with maven-publish and publishing block

--- SmartAds/build.gradle.kts
+++ SmartAds/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
@@ -8,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 23
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -22,23 +22,46 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    publishing {
+        singleVariant("release")
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-
     implementation("com.google.android.gms:play-services-ads:24.5.0")
     implementation("com.facebook.android:audience-network-sdk:6.+")
     implementation("androidx.lifecycle:lifecycle-runtime:2.9.2")
     implementation("androidx.lifecycle:lifecycle-process:2.9.2")
+}
+
+// ✅ Important: wrap publishing block inside afterEvaluate
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.partharoypc"
+                artifactId = "SmartAds"
+                version = "1.0.1"
+
+                pom {
+                    name.set("SmartAds")
+                    description.set("Simple ad mediation library for Android (AdMob + Facebook)")
+                    url.set("https://github.com/partharoypc/SmartAds")
+                }
+            }
+        }
+    }
 }
